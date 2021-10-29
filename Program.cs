77c03d88f7c33f3382aa72fb08c297267ab159f1
kaiorod <caiorodrigo2775@gmail.com>
2021-10-29 20:49:46 -0300

    Finaliza Senha

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Senha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome, senha;
+
+            Console.Beep();
+            Console.Clear();
+            Console.Write("Digite seu nome : ");
+            nome = Console.ReadLine();
+
+            if(nome != "")
+            {
+                Console.Write($"Olá {nome}, digite sua senha : ");
+                senha = Console.ReadLine();
+
+                if(senha == "1234abcd")
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine("\nAcesso Permitido");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("\nAcesso Negado");
+                    Console.ResetColor();
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Digite o nome de usuário!!");
+                Console.ResetColor();
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Senha
-Solicite ao usuário que digite a sua senha (uma string). Exiba "Acesso permitido" caso a senha digitada seja 1234abcd, senão exiba "Acesso negado".  Ex.:  Olá, usuário. Por favor, digite sua senha: minha_senha_super_secreta Acesso negado ou  Olá, usuário. Por favor, digite sua senha: 1234abcd Acesso permitido
+Solicite ao usuário que digite a sua senha (uma string). Exiba "Acesso permitido" caso a senha digitada seja 1234abcd, senão exiba "Acesso negado". 
+Ex.:  Olá, usuário. Por favor, digite sua senha: minha_senha_super_secreta 
+Acesso negado 
+ou  
+Olá, usuário. Por favor, digite sua senha: 1234abcd
+Acesso permitido
